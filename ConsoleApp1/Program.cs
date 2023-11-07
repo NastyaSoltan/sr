956c66cd8135947c3110f99e297c73f12d433fb2
NastyaSoltan <122930481+NastyaSoltan@users.noreply.github.com>
2023-11-08 01:07:30 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {//реализовать функцию вычисления частного двух целых чисел
+            Console.Write("Введите первое число: ");
+            int dividend = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int divider = int.Parse(Console.ReadLine());
+            Result(dividend, divider);
+        }
+        static void Result(int dividend, int divider)
+        {
+            int quotient = dividend / divider;
+            Console.WriteLine($"{dividend} делить на {divider} равно {quotient}");
+            Console.ReadKey();
+        }
+    }
+        
+}
